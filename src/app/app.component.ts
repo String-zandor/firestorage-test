e826acdfc3178bc6e6f27244deba42bd602cd2c5
String-zandor <zwritescode@gmail.com>
2023-01-30 11:02:15 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firestorage-test';
+
+  url: string = "";
+  urlArray: string[] = [];
+
+  constructor(private storage: AngularFireStorage) {
+
+  }
+
+  uploadFile(event: any) {
+    const files: FileList = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+      const fileName: string = files[i].name;
+      const task = this.storage.upload(fileName, files[i]);
+      task.then(() => {
+        this.storage.ref(fileName).getDownloadURL().subscribe(url => {
+          this.urlArray.push(url);
+        })
+      });
+    }
+  }
+
+
+  
+  
+
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAeXqjOJAz-S_aI-plNnPgetL3SUN0qtjs",
+        authDomain: "firestorage-test-a7a66.firebaseapp.com",
+        projectId: "firestorage-test-a7a66",
+        storageBucket: "firestorage-test-a7a66.appspot.com",
+        messagingSenderId: "488096599997",
+        appId: "1:488096599997:web:c460beb290a979ca964cd4"
+    }
+}
